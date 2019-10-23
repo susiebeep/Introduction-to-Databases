5fe8e1e7d36b7b69b47e8fdf372300c3fe88b53b
Susan <hibberts@oregonstate.edu>
2019-10-23 15:56:04 -0700

    Add files via upload

---
+++ myserver.py
@@ -0,0 +1,18 @@
+#import mysql.connector
+#mydb = mysql.connector.connect(
+	#host='classmysql.engr.oregonstate.edu',
+	#user='cs340_hibberts',
+	#passwd='3975',
+	#database='cs340_hibberts')
+
+#mycursor = mydb.cursor()
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3975)
