d8a2aa61eb5fcdb1930b0182f888725ef32311f3
keenonhh <47375187+keenonhh@users.noreply.github.com>
2019-10-23 18:56:08 -0700

    Add files via upload

--- myserver.py
+++ myserver.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('album')
+@app.route('/album')
 def album():
     return render_template('add_album.html')
-    
+
+@app.route('/track')
+def track():
+    return render_template('add_track.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3975)
