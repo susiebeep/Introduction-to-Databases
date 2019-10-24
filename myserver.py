78636acfb5bae75c755b599d687e1f418195f622
keenonhh <47375187+keenonhh@users.noreply.github.com>
2019-10-23 18:59:38 -0700

    Add files via upload

--- myserver.py
+++ myserver.py
@@ -13,5 +13,9 @@ def album():
 def track():
     return render_template('add_track.html')
 
+@app.route('/band_members')
+def band_members():
+    return render_template('band_members.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3975)
