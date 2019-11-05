a4aee2f79f7b760980c801b7ae2e345c87bd2ee7
keenonhh <47375187+keenonhh@users.noreply.github.com>
2019-11-05 15:36:34 -0800

    Update db_credentials.py

--- db_credentials.py
+++ db_credentials.py
@@ -5,6 +5,6 @@
 # in turn by all the Python code in this codebase to interact with the database
 
 host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = 'cs340_hunsakke' #CHANGE ME
-passwd = 'Keenxenxan123!' #CHANGE ME
-db = 'cs340_hunsakke' #CHANGE ME
+user = 'cs340_' #CHANGE ME
+passwd = '' #CHANGE ME
+db = 'cs340_' #CHANGE ME
