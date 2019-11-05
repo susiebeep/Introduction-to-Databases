c75536b8a438c6e176cac4e4d7710ba892f8f189
Susan <hibbert.susan@gmail.com>
2019-11-05 14:17:08 -0800

    modified web pages to display back-end DB

--- myserver.py
+++ myserver.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask import request
-from db_connector.db_connector import connect_to_database, execute_query
+from db_connector import connect_to_database, execute_query
 
 app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
@@ -11,34 +11,63 @@ def index():
 
 @app.route('/album')
 def album():
-    return render_template('album.html')
+    db_connection = connect_to_database()
+    query = 'SELECT * FROM `album`';
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('album.html', rows = result)
 
 @app.route('/track')
 def track():
-    return render_template('track.html')
+    db_connection = connect_to_database()
+    query = 'SELECT * FROM `tracks`';
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('track.html', rows = result)
 
 @app.route('/band_members')
 def band_members():
-    return render_template('members.html')
+    db_connection = connect_to_database()
+    query = 'SELECT * FROM `band members`';
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('members.html', rows = result)
 
 @app.route('/shows')
 def shows():
-    return render_template('shows.html')
+    db_connection = connect_to_database()
+    query = 'SELECT * FROM `shows`';
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('shows.html', rows = result)
 
 @app.route('/set_list')
 def set_list():
-    return render_template('set_list.html')
+    db_connection = connect_to_database()
+    query = 'SELECT * FROM `set list`';
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('set_list.html', rows = result)
+    
+@app.route('/track_contributors')
+def track_contributors():
+    db_connection = connect_to_database()
+    query = 'SELECT * FROM `track contributor`';
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('track_contributors.html', rows = result)
+    
 
 @app.route('/add_album', methods = ['POST', 'GET'])
 def add_album():
-    Album_Name = request.form['Album_Name']
-    Release_Date = request.form['Release_Date']
-    query = 'INSERT INTO `album` (`album name`, `release date`) VALUES (:Album_Name, :Release_Date)'
-    data = (Album_Name, Release_Date)
-    db_connection = connect_to_database()
-    execute_query(db_connection, query, data)
-    return("Album added!");
-    #return render_template('add_album.html')
+    #Album_Name = request.form['Album_Name']
+    #Release_Date = request.form['Release_Date']
+    #query = 'INSERT INTO `album` (`album name`, `release date`) VALUES (:Album_Name, :Release_Date)'
+    #data = (Album_Name, Release_Date)
+    #db_connection = connect_to_database()
+    #execute_query(db_connection, query, data)
+    #return("Album added!");
+    return render_template('add_album.html')
 
 @app.route('/add_track')
 def add_track():
@@ -56,9 +85,7 @@ def add_shows():
 def add_set_list():
     return render_template('add_set_list.html')
 
-@app.route('/track_contributors')
-def track_contributors():
-    return render_template('track_contributors.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3975)
