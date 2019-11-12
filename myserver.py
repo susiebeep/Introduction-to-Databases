0807950e9a5dc1f1b86d80bbd3e9a62d20fc73c5
Susan <hibbert.susan@gmail.com>
2019-11-12 15:09:14 -0800

    trying to get line up id to appear in dropdown

--- myserver.py
+++ myserver.py
@@ -80,17 +80,27 @@ def add_track():
     
 @app.route('/add_track_new', methods = ['POST', 'GET'])
 def add_track_new():
-    track_name = request.form['Track_Name']
-    track_length = request.form['Release_Date']
-    album_id = request.form['Album_ID']
-    band_member_id = request.form['Band_Member_ID']
-    query = 'INSERT INTO `tracks` (`track name`, `track length`, `album id`) VALUES (%s, %s, %s)'
-    #will need to insert the band member id and track id into track contributors table
-    data = (track_name, track_length, album_id)
-    print(data)
     db_connection = connect_to_database()
-    execute_query(db_connection, query, data)
-    return render_template('add_track_new.html')    
+    
+    if request.method == 'GET':
+        query = 'SELECT `band member id` FROM `band members`'
+        result = execute_query(db_connection, query).fetchall();
+        print(result)
+        return render_template('add_track.html', rows = result)   
+    
+    #elif request.method == 'POST':
+    
+        #track_name = request.form['Track_Name']
+        #track_length = request.form['Release_Date']
+        #album_id = request.form['Album_ID']
+        #band_member_id = request.form['Band_Member_ID']
+        #query = 'INSERT INTO `tracks` (`track name`, `track length`, `album id`) VALUES (%s, %s, %s)'
+        #will need to insert the band member id and track id into track contributors table
+        #data = (track_name, track_length, album_id)
+        #print(data)
+        #execute_query(db_connection, query, data)
+        #return render_template('add_track_new.html')   
+    
 
 @app.route('/add_band_members')
 def add_band_members():
