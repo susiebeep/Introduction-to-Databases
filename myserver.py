6efb0a39af64b755db4b959795810d4df7f2f276
Susan <hibbert.susan@gmail.com>
2019-11-15 14:59:08 -0800

    working search function

--- myserver.py
+++ myserver.py
@@ -156,7 +156,7 @@ def add_track_contributors_new():
 def search():
     db_connection = connect_to_database()
     track_id = request.form['Search']
-    query = 'SELECT `tracks`.`track id`, `tracks`.`track name`, `tracks`.`track length`, `tracks`.`album id`, `band members`.`name`, `shows`.`line up id`, `shows`.`city` FROM `tracks` JOIN `set list` ON `tracks`.`track id` = `set list`.`track id` JOIN `shows` ON `set list`.`line up id` = `shows`.`line up id` JOIN `track band member` ON `tracks`.`track id` = `track band member`.`track id` JOIN `band members` ON `track band member`.`band member id` = `band members`.`band member id` WHERE `tracks`.`track id` = (%s)'
+    query = 'SELECT `tracks`.`track id`, `tracks`.`track name`, `tracks`.`track length`, `tracks`.`album id`, `band members`.`name`, `shows`.`line up id`, `shows`.`city` FROM `tracks` LEFT JOIN `set list` ON `tracks`.`track id` = `set list`.`track id` LEFT JOIN `shows` ON `set list`.`line up id` = `shows`.`line up id` LEFT JOIN `track band member` ON `tracks`.`track id` = `track band member`.`track id` LEFT JOIN `band members` ON `track band member`.`band member id` = `band members`.`band member id` WHERE `tracks`.`track id` = (%s)'
     data = (track_id,)
     result_search = execute_query(db_connection, query, data).fetchall();
     return render_template('results.html', results = result_search)    
